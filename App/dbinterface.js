b42d0230dd160c560637c25fe0038dbf20ec85e1
rldail <56740747+rldail@users.noreply.github.com>
2020-08-17 08:44:03 -0500

    Update dbinterface.js

--- App/dbinterface.js
+++ App/dbinterface.js
@@ -53,6 +53,8 @@ var executeQuery = function(res, query) {
 */
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, POST, PUT');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
